6d92e32e007e744f76d8c77ce80f2ee0109a3827
Data Juggler <corby@datajuggler.com>
2019-11-11 15:15:24 -0600

    I created a class called UploadedFileInfo that returns more information about a file that was uploaded than just the name.

---
+++ BlazorInputFile/UploadedFileInfo.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BlazorInputFile
+{
+    public class UploadedFileInfo
+    {
+        #region Private Variables
+        private string fileName;
+        private string partialGuid;
+        private DateTime lastModified;
+        private string name;
+        private long size;
+        private string type;
+        #endregion
+
+        #region Properties
+
+            #region FileName
+            /// <summary>
+            /// This property gets or sets the value for 'FileName'.
+            /// </summary>
+            public string FileName
+            {
+                get { return fileName; }
+                set { fileName = value; }
+            }
+            #endregion
+
+            #region FileNameWithPartialGuid
+            /// <summary>
+            /// This read only property returns the FileName plus the PartialGuid.
+            /// </summary>
+            public string FileNameWithPartialGuid
+            {
+                get
+                {
+                    // initial value
+                    string fileNameWithPartialGuid = FileName + "." + PartialGuid;
+
+                    // return value
+                    return fileNameWithPartialGuid;
+                }
+            }
+            #endregion
+            
+            #region LastModified
+            /// <summary>
+            /// This property gets or sets the value for 'LastModified'.
+            /// </summary>
+            public DateTime LastModified
+            {
+                get { return lastModified; }
+                set { lastModified = value; }
+            }
+            #endregion
+            
+            #region Name
+            /// <summary>
+            /// This property gets or sets the value for 'Name'.
+            /// </summary>
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            #endregion
+            
+            #region PartialGuid
+            /// <summary>
+            /// This property gets or sets the value for 'PartialGuid'.
+            /// </summary>
+            public string PartialGuid
+            {
+                get { return partialGuid; }
+                set { partialGuid = value; }
+            }
+            #endregion
+            
+            #region Size
+            /// <summary>
+            /// This property gets or sets the value for 'Size'.
+            /// </summary>
+            public long Size
+            {
+                get { return size; }
+                set { size = value; }
+            }
+            #endregion
+            
+            #region Type
+            /// <summary>
+            /// This property gets or sets the value for 'Type'.
+            /// </summary>
+            public string Type
+            {
+                get { return type; }
+                set { type = value; }
+            }
+            #endregion
+            
+        #endregion
+
+    }
+}
---
+++ DataJuggler.Blazor.FileUpload/UploadedFileInfo.cs
@@ -0,0 +1,117 @@
+﻿using BlazorInputFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataJuggler.Blazor.FileUpload
+{
+    public class UploadedFileInfo
+    {
+        #region Private Variables
+        private string partialGuid;
+        private DateTime lastModified;
+        private string name;
+        private long size;
+        private string type;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Create a new instance of an UploadedFileInfo object
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="partialGuid"></param>
+        public UploadedFileInfo(IFileListEntry file, string partialGuid)
+        {
+            // verify both objects exist
+            if ((file != null) && (!String.IsNullOrWhiteSpace(partialGuid)))
+            {
+                // Set all the properties
+                this.LastModified = file.LastModified;
+                this.Name = file.Name;
+                this.PartialGuid = partialGuid;
+                this.Size = file.Size;
+                this.Type = file.Type;
+            }
+        }
+        #endregion
+
+        #region Properties
+            
+            #region LastModified
+            /// <summary>
+            /// This property gets or sets the value for 'LastModified'.
+            /// </summary>
+            public DateTime LastModified
+            {
+                get { return lastModified; }
+                set { lastModified = value; }
+            }
+            #endregion
+            
+            #region Name
+            /// <summary>
+            /// This property gets or sets the value for 'Name'.
+            /// </summary>
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            #endregion
+
+            #region NameWithPartialGuid
+            /// <summary>
+            /// This read only property returns the File Name plus the PartialGuid.
+            /// </summary>
+            public string NameWithPartialGuid
+            {
+                get
+                {
+                    // initial value
+                    string nameWithPartialGuid = Name + "." + PartialGuid;
+
+                    // return value
+                    return nameWithPartialGuid;
+                }
+            }
+            #endregion
+            
+            #region PartialGuid
+            /// <summary>
+            /// This property gets or sets the value for 'PartialGuid'.
+            /// </summary>
+            public string PartialGuid
+            {
+                get { return partialGuid; }
+                set { partialGuid = value; }
+            }
+            #endregion
+            
+            #region Size
+            /// <summary>
+            /// This property gets or sets the value for 'Size'.
+            /// </summary>
+            public long Size
+            {
+                get { return size; }
+                set { size = value; }
+            }
+            #endregion
+            
+            #region Type
+            /// <summary>
+            /// This property gets or sets the value for 'Type'.
+            /// </summary>
+            public string Type
+            {
+                get { return type; }
+                set { type = value; }
+            }
+            #endregion
+            
+        #endregion
+
+    }
+}
